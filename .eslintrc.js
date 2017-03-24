9c3fedc1aad9dbda2475ee3290515d10ca59b763
Dakota Raymond <dakotalraymond@gmail.com>
2017-03-24 13:10:47 -0600

    Added Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "google",
+    "globals": {
+        "response": true,
+        "process": true
+    },
+    "rules": {
+        "undeclared_a": 0
+    }
+};
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', function (request, response) {
+  response.send('Hello World!');
+});
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
