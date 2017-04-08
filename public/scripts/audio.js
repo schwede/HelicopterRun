d2530ae005c38a8d4b6c0d77fc82ce136a4f1f03
swede <swede.struve@aggiemail.usu.edu>
2017-04-07 21:30:30 -0600

    Basic html audio wrapper for sounds

---
+++ public/scripts/audio.js
@@ -0,0 +1,28 @@
+
+function SoundEffect(spec) {
+  let that = {};
+
+  let audoObj = new Audio(spec.source);
+
+  if(spec.loop != null) {
+    audoObj.loop = spec.loop;
+  } else {
+    audoObj.loop = false;
+  }
+
+  if(spec.volume != null) {
+    audoObj.volume = spec.volume;
+  } else {
+    audoObj.volume = 0.1;
+  }
+
+  that.play = () => {
+    audoObj.play();
+  };
+
+  that.pause = () => {
+    audoObj.pause();
+  };
+
+  return that;
+}
